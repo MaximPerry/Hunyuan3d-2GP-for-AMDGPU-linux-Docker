c01f6a9a8380d41417930b230fafe2671f982d6a
MaximPerry <maxim_perry@live.ca>
2025-09-28 19:39:36 -0400

    Updated guide

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,19 +4,6 @@
 echo "Enter your AMD GPU name (e.g.: gfx1102): "
 read gpu
 
-#Use specific version of python
-#tar xvf python/py310.tar.xz
-#mv Python-3.10.17 py310
-#export PATH="$PWD/py310/bin:$PATH"
-
-#Use specific version of python
-#tar xvf python/py310.tar.xz
-#mv Python-3.10.17 py310
-#cd py310
-#./configure --prefix=$PWD/py310 --enable-optimizations
-#make -j$(nproc)
-#make install
-#cd ..
 
 #Compile custom_rasterizer module specific to this container
 apt-get update
@@ -38,21 +25,18 @@ python3 setup.py install
 cd ..
 
 #Clone and configure original repo
-git clone https://github.com/Tencent-Hunyuan/Hunyuan3D-2.git
-cd Hunyuan3D-2
+git clone https://github.com/deepbeepmeep/Hunyuan3D-2GP.git
+cd Hunyuan3D-2GP
 python3 -m pip install -e .
 cd hy3dgen/texgen/differentiable_renderer/
 python3 setup.py install
 
-#Custom python wheels for AMD 
-#python3 -m pip install ../wheels/custom_rasterizer-0.1-py310-none-manylinux_2_39_x86_64.whl
-#python3 -m pip install gradio==5.33.0 opencv-python==4.9.0.80 opencv-python-headless==4.10.0.84 numpy==1.26.4
-#python3 -m pip install jmespath 
-#cd ..
 
-# Build & install custom_rasterizer from source instead of using prebuilt wheel
-python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install .
+# Build & install custom_rasterizer (this replaces the prebuilt wheel!)
+cd ../custom_rasterizer/
+python3 setup.py bdist_wheel
+python3 -m pip install dist/*.whl
+
 cd ../../../../
 
 # Other Python deps
@@ -63,19 +47,19 @@ echo "What port do you want this app to use?"
 read port
 
 #Create hunyuan.sh
-echo -e \#\!"/bin/bash\nexport FLASH_ATTENTION_TRITON_AMD_ENABLE=\"TRUE\"\nexport GPU_ARCHS=\"$gpu\"\npython3 gradio_app.py --model_path tencent/Hunyuan3D-2 --subfolder hunyuan3d-dit-v2-0-turbo --texgen_model_path tencent/Hunyuan3D-2 --low_vram_mode --enable_flashvdm --enable_t23d --port $port" >> hunyuan.sh
+echo -e \#\!"/bin/bash\nexport FLASH_ATTENTION_TRITON_AMD_ENABLE=\"TRUE\"\nexport GPU_ARCHS=\"$gpu\"\npython3 gradio_app.py --model_path tencent/Hunyuan3D-2GP --subfolder hunyuan3d-dit-v2-0-turbo --texgen_model_path tencent/Hunyuan3D-2GP --low_vram_mode --enable_flashvdm --enable_t23d --port $port" >> hunyuan.sh
 chmod +x hunyuan.sh
 
 #Create hunyuan-mv.sh
-echo -e \#\!"/bin/bash\nexport FLASH_ATTENTION_TRITON_AMD_ENABLE=\"TRUE\"\nexport GPU_ARCHS=\"$gpu\"\nport=\`cat ../port\`\npython3 gradio_app.py --model_path tencent/Hunyuan3D-2mv --subfolder hunyuan3d-dit-v2-mv-turbo --texgen_model_path tencent/Hunyuan3D-2 --low_vram_mode --enable_flashvdm --port $port" >> hunyuan-mv.sh
+echo -e \#\!"/bin/bash\nexport FLASH_ATTENTION_TRITON_AMD_ENABLE=\"TRUE\"\nexport GPU_ARCHS=\"$gpu\"\nport=\`cat ../port\`\npython3 gradio_app.py --model_path tencent/Hunyuan3D-2mv --subfolder hunyuan3d-dit-v2-mv-turbo --texgen_model_path tencent/Hunyuan3D-2GP --low_vram_mode --enable_flashvdm --port $port" >> hunyuan-mv.sh
 chmod +x hunyuan-mv.sh
 
 #Clean up
 rm -rf wheels
 rm -rf flash-attention
-mv -f gradio_app.py Hunyuan3D-2/gradio_app.py
-mv -f hunyuan.sh Hunyuan3D-2/hunyuan.sh
-mv -f hunyuan-mv.sh Hunyuan3D-2/hunyuan-mv.sh
+#mv -f gradio_app.py Hunyuan3D-2GP/gradio_app.py
+mv -f hunyuan.sh Hunyuan3D-2GP/hunyuan.sh
+mv -f hunyuan-mv.sh Hunyuan3D-2GP/hunyuan-mv.sh
 rm  requirements.txt
 rm  README.md
 rm  INSTALL.sh
