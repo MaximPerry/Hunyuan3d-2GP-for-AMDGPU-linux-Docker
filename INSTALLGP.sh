9d9b176b7953c7121fb9ec4a21e3516a1f9d70dc
MaximPerry <maxim_perry@live.ca>
2025-09-29 20:48:21 -0400

    Updated guide

--- INSTALLGP.sh
+++ INSTALLGP.sh
@@ -26,8 +26,8 @@ python3 setup.py install
 cd ..
 
 #Clone and configure original repo
-git clone https://github.com/Tencent-Hunyuan/Hunyuan3D-2.git
-cd Hunyuan3D-2
+git clone https://github.com/kmykprn/Hunyuan3D-2GP.git
+cd Hunyuan3D-2GP
 python3 -m pip install -e .
 cd hy3dgen/texgen/differentiable_renderer/
 python3 setup.py install
@@ -48,20 +48,24 @@ echo "What port do you want this app to use?"
 read port
 
 #Create hunyuan.sh
-echo -e \#\!"/bin/bash\nexport FLASH_ATTENTION_TRITON_AMD_ENABLE=\"TRUE\"\nexport GPU_ARCHS=\"$gpu\"\npython3 gradio_app.py --model_path tencent/Hunyuan3D-2GP --subfolder hunyuan3d-dit-v2-0-turbo --texgen_model_path tencent/Hunyuan3D-2GP --low_vram_mode --enable_flashvdm --enable_t23d --port $port" >> hunyuan.sh
+#echo -e \#\!"/bin/bash\nexport FLASH_ATTENTION_TRITON_AMD_ENABLE=\"TRUE\"\nexport GPU_ARCHS=\"$gpu\"\npython3 gradio_app.py --model_path tencent/Hunyuan3D-2GP --subfolder hunyuan3d-dit-v2-0-turbo --texgen_model_path tencent/Hunyuan3D-2GP --low_vram_mode --enable_flashvdm --enable_t23d --port $port" >> hunyuan.sh
+echo "Which memory saving profile do you want to use? (1-5): "
+read profile
+
+echo -e \#\!"/bin/bash\nexport FLASH_ATTENTION_TRITON_AMD_ENABLE=\"TRUE\"\nexport GPU_ARCHS=\"$gpu\"\npython3 gradio_app.py --profile $profile --turbo --enable_flashvdm --enable_t23d --port $port" >> hunyuan.sh
 
 chmod +x hunyuan.sh
 
 #Create hunyuan-mv.sh
-echo -e \#\!"/bin/bash\nexport FLASH_ATTENTION_TRITON_AMD_ENABLE=\"TRUE\"\nexport GPU_ARCHS=\"$gpu\"\npython3 gradio_app.py --model_path tencent/Hunyuan3D-2mv --subfolder hunyuan3d-dit-v2-mv-turbo --texgen_model_path tencent/Hunyuan3D-2 --low_vram_mode --enable_flashvdm --port $port" >> hunyuan-mv.sh
+echo -e \#\!"/bin/bash\nexport FLASH_ATTENTION_TRITON_AMD_ENABLE=\"TRUE\"\nexport GPU_ARCHS=\"$gpu\"\npython3 gradio_app.py --model_path tencent/Hunyuan3D-2GPmv --subfolder hunyuan3d-dit-v2-mv-turbo --texgen_model_path tencent/Hunyuan3D-2GP --low_vram_mode --enable_flashvdm --port $port" >> hunyuan-mv.sh
 chmod +x hunyuan-mv.sh
 
 #Clean up
 rm -rf wheels
 rm -rf flash-attention
-mv -f gradio_app.py Hunyuan3D-2/gradio_app.py
-mv -f hunyuan.sh Hunyuan3D-2/hunyuan.sh
-mv -f hunyuan-mv.sh Hunyuan3D-2/hunyuan-mv.sh
+mv -f gradio_app.py Hunyuan3D-2GP/gradio_app.py
+mv -f hunyuan.sh Hunyuan3D-2GP/hunyuan.sh
+mv -f hunyuan-mv.sh Hunyuan3D-2GP/hunyuan-mv.sh
 rm  requirements.txt
 rm  README.md
 rm  INSTALL.sh
