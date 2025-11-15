ea0a0624c46492af1885645935ca30fe3636dec1
MaximPerry <maxim_perry@live.ca>
2025-11-14 19:12:31 -0500

    Updated guide

--- INSTALL.sh
+++ INSTALL.sh
@@ -12,7 +12,7 @@ apt-get install -y build-essential python3.10-dev
 
 #Install general requirements
 python3 -m ensurepip
-#python3 -m pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/rocm6.4
+python3 -m pip install torch==2.7.1 torchvision==0.22.1 torchaudio==2.7.1 --index-url https://download.pytorch.org/whl/rocm6.3
 python3 -m pip install -r requirements.txt
 python3 -m pip install mmgp
 
